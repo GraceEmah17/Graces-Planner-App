113414f302c453b4604a7008b1116181ea0d3826
Grace Emah <grace_esueti@yahoo.co.uk>
2024-01-09 15:42:04 +0000

    added the function to save events to local storage

--- starter/scripts.js
+++ starter/scripts.js
@@ -5,4 +5,26 @@ $(document).ready(function () {
       $("h1").text(`Work Day Scheduler - ${currentDate}`);
     }
 
-    
+    // Function to load events from local storage
+  function loadEvents() {
+    const events = JSON.parse(localStorage.getItem("events")) || {};
+
+    for (const hour in events) {
+      const eventText = events[hour];
+      $(`#${hour}`).val(eventText);
+    }
+  }
+  
+  // Function to save events to local storage
+  function saveEvents() {
+    const events = {};
+
+    $(".event input").each(function () {
+      const hour = $(this).attr("id");
+      const eventText = $(this).val();
+      events[hour] = eventText;
+    });
+
+    localStorage.setItem("events", JSON.stringify(events));
+  }
+
